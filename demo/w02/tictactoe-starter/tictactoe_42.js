3d26391a0f2d7df13063191bdd141d1337374fce
George0113 <a28935617@gmail.com>
2023-02-23 20:34:45 +0800

    W02-P2: O win (9 times), X wins (8 times), tie (9 times)

--- demo/w02/tictactoe-starter/tictactoe_42.js
+++ demo/w02/tictactoe-starter/tictactoe_42.js
@@ -1,7 +1,15 @@
+
+const o = 'o';
+const x = 'x';
+let turn = 0;
+let done = false;
+
 const resetBtn = document.querySelector('#reset');
 
 const allLi = document.querySelectorAll('#board li');
 console.log('allLi', allLi);
+
+const container = document.querySelector('#container');
 
 const checkWin = (player) => {
 
@@ -19,8 +27,10 @@ const checkWin = (player) => {
         (p4 && p5 && p6) ||
         (p7 && p8 && p9) ||
         (p1 && p5 && p9) ||
-        (p3 && p5 && p7)
-
+        (p3 && p5 && p7) ||
+        (p1 && p4 && p7) ||
+        (p2 && p5 && p8) ||
+        (p3 && p6 && p9)
         )
         return true;
     else
@@ -31,10 +41,52 @@ const reset = () => {
     allLi.forEach( (item) => {
         item.textContent = '+';
         item.classList = '';
+        container.style.backgroundColor = '#666';
+        turn = 0;
+        done = false;
     } );
-} 
+}
 
-console.log('checkWin(o)',checkWin('o'));
-console.log('checkWin(x)',checkWin('x'));
+const winMessage = (player) =>{
+    if(player === 'o'){
+        container.style.backgroundColor = 'rgba(144,238,144,0.5)';
+    }else {
+        container.style.backgroundColor = 'rgba(240,118,128,0.726)';
+    }
+    alert(`player ${player} wins`);
+
+}
+
+allLi.forEach((item) =>{
+    item.addEventListener('click', () => {
+        if(item.classList.contains('disabled')){
+            alert('Already Filled');
+        }else if(turn % 2 === 0){
+            item.textContent = 'O';
+            item.classList.add('o','disabled');
+            if(checkWin('o')){
+                winMessage(o);
+                done = true;
+            }
+        }else if ( turn % 2 === 1){
+            item.textContent = 'X';
+            item.classList.add('x','disabled');
+            if(checkWin('x')){
+                winMessage(x);
+                done = true;
+            }
+        }
+
+        if(!done && turn < 8){
+            turn++;
+        }else if(turn >=8){
+            alert('tie');
+        }
+    })
+});
+
+
+// console.log('checkWin(o)',checkWin('o'));
+// console.log('checkWin(x)',checkWin('x'));
 
 resetBtn.addEventListener('click', reset);
