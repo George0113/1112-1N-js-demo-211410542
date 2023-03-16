ff49cc4e9114a29da56e073cbeb09e2dfc4da66e
George0113 <a28935617@gmail.com>
2023-03-16 18:44:12 +0800

    W05-P1: use JavaScript module to import and export

--- demo/app_42.js
+++ demo/app_42.js
@@ -1,66 +1,4 @@
-const menu = [
-    {
-        id: 1,
-        title:'hot dog',
-        category:'breakfast',
-        price:15.59,
-        img:'./images/hotdog.jpg',
-        remote_img:'',
-        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
-
-    },
-    {
-        id: 2,
-        title:'Hamburger',
-        category:'lunch',
-        price:150.59,
-        img:'./images/Hamburger.jpg',
-        remote_img:'',
-        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
-
-    },
-    {
-        id: 3,
-        title:'steak',
-        category:'dinner',
-        price:1500.59,
-        img:'./images/11.jpg',
-        remote_img:'',
-        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
-
-    },
-    {
-        id: 4,
-        title:'buttermilk pancakes',
-        category:'breakfast',
-        price:15.59,
-        img:'./images/item-1.jpeg',
-        remote_img:'',
-        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
-
-    },
-    {
-        id: 5,
-        title:'dinner double',
-        category:'dessert',
-        price:13.99,
-        img:'./images/item-2.jpeg',
-        remote_img:'',
-        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
-
-    },
-    {
-        id: 6,
-        title:'godzilla milkshake',
-        category:'shakes',
-        price:15.59,
-        img:'./images/item-3.jpeg',
-        remote_img:'',
-        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
-
-    },
-];
-
+import { menu } from'./data.js'
 
 const sectionCenter = document.querySelector('.section-center');
 const btnContainer = document.querySelector('.btn-container');
@@ -80,7 +18,7 @@ const displayMenuItems = (menu) => {
             </p>
           </div>
         </article>
-        
+
         `
     });
     // console.log('displayMenu before join',displayMenu);
---
+++ demo/data.js
@@ -0,0 +1,62 @@
+export const menu = [
+    {
+        id: 1,
+        title:'hot dog',
+        category:'breakfast',
+        price:15.59,
+        img:'./images/hotdog.jpg',
+        remote_img:'',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
+
+    },
+    {
+        id: 2,
+        title:'Hamburger',
+        category:'lunch',
+        price:150.59,
+        img:'./images/Hamburger.jpg',
+        remote_img:'',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
+
+    },
+    {
+        id: 3,
+        title:'steak',
+        category:'dinner',
+        price:1500.59,
+        img:'./images/11.jpg',
+        remote_img:'',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
+
+    },
+    {
+        id: 4,
+        title:'buttermilk pancakes',
+        category:'breakfast',
+        price:15.59,
+        img:'./images/item-1.jpeg',
+        remote_img:'',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
+
+    },
+    {
+        id: 5,
+        title:'dinner double',
+        category:'dessert',
+        price:13.99,
+        img:'./images/item-2.jpeg',
+        remote_img:'',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
+
+    },
+    {
+        id: 6,
+        title:'godzilla milkshake',
+        category:'shakes',
+        price:15.59,
+        img:'./images/item-3.jpeg',
+        remote_img:'',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non qui recusandae natus, repudiandae voluptates quasi aperiam temporibus sint labore provident beatae ut esse perspiciatis deserunt, atque magni! Nemo, culpa!',
+
+    },
+];
