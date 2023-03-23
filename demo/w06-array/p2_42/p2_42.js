2d3367e935df97de9d73d7f56859e650762fe392
George0113 <a28935617@gmail.com>
2023-03-23 21:57:58 +0800

    W06-P4: compute the average of students and sdata array

--- demo/w06-array/p2_42/p2_42.js
+++ demo/w06-array/p2_42/p2_42.js
@@ -11,6 +11,8 @@ console.log(`The lowest score: ${sdata[sdata.length-1]}`);
 
 
 
+
+
 console.log('sdata', sdata);
 
 const students2 = students.map((student)=>{
@@ -26,3 +28,20 @@ console.log('students2',students2);
 
 console.log(`The lowest score: ${students2[0].score}`);
 console.log(`The hightest score: ${students2[students2.length-1].score}`);
+
+
+console.log('students',students);
+const averageStudents = students.reduce((total, student, index) => {
+    console.log('index total',index,total);
+    return total + student.score;
+}, 0) / students.length;
+
+console.log('averageStudents',averageStudents);
+
+console.log('sdata',sdata);
+const averageSdata = sdata.reduce((total, student,index)=>{
+    console.log('index total',index,total);
+    return total + student;
+}, 0) /sdata.length;
+
+console.log('averageSdata',averageSdata);
--- demo/w06-array/p3_42/p3_42.js
+++ demo/w06-array/p3_42/p3_42.js
@@ -1,4 +1,37 @@
-let c, f;
-c = Number(prompt("Enter temperature in C:")).toFixed(2);
-f =(c* 9/5+32).toFixed(2);
-console.log(`${c} C = ${f} F`);
+const userInput = document.querySelector('#input-number');
+const cBtn = document.querySelector('#btn-c');
+const fBtn = document.querySelector('#btn-f');
+const result = document.querySelector('#current-calculation');
+
+
+
+
+
+cBtn.addEventListener('click',()=>{
+    const f = (getUserInput()*9/5+32);
+    console.log('123',f);
+    OutputC(f);
+});
+
+
+
+fBtn.addEventListener('click',()=>{
+    const f = (getUserInput()*5/9-32);
+    OutputF(f);
+});
+
+
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function OutputC(text)
+{
+    result.textContent = `${Number(userInput.value).toFixed(2)} C = ${text.toFixed(2)}F`;
+}
+
+function OutputF(text)
+{
+    result.textContent = `${Number(userInput.value).toFixed(2)} F = ${text.toFixed(2)}C`;
+}
+console.log('btn-c',cBtn);
