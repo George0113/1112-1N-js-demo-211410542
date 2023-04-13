f62fd90cd8b7108ff93d0dd566426e98428ef3fe
George0113 <a28935617@gmail.com>
2023-04-13 21:43:19 +0800

    W09-P3: Midprep P1 answer (c)(d)

--- js/classdemo.js
+++ js/classdemo.js
@@ -1,11 +1,11 @@
 const showClassDemo = (week) => {
-    const showDome = document.querySelector('.demo-section');
+    const showDome = document.querySelector('.banner-section');
     switch(week){
         case 'w1':
-            showDome.innerHTML = `<iframe src='./demo/w01/index.html' width="100% height"100vh"/>`;
+            showDome.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height"100vh"/>`;
             break;
         case 'w2':
-            showDome.innerHTML = `<iframe src='./demo/w02/index.html' width="100% height"100vh"/>`;
+            showDome.innerHTML = `<iframe src='./demo/w02/index.html' width="100%" height"100vh"/>`;
             break;
     }
 
