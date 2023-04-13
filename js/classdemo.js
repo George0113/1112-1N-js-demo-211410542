656bd4a146d8aab4fef5f8d12f1b31efa8382e08
George0113 <a28935617@gmail.com>
2023-04-13 21:35:01 +0800

    w9_demo

--- js/classdemo.js
+++ js/classdemo.js
@@ -5,6 +5,7 @@ const showClassDemo = (week) => {
             showDome.innerHTML = `<iframe src='./demo/w01/index.html' width="100% height"100vh"/>`;
             break;
         case 'w2':
+            showDome.innerHTML = `<iframe src='./demo/w02/index.html' width="100% height"100vh"/>`;
             break;
     }
 
