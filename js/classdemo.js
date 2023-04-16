8a90956cbe711c35307ca88546eda93929aaf6c7
George0113 <a28935617@gmail.com>
2023-04-16 12:15:23 +0800

    Newo

--- js/classdemo.js
+++ js/classdemo.js
@@ -4,17 +4,32 @@ const showClassDemo = (week) => {
         case 'w1':
             showDome.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100%"/>`;
             break;
+        case 'w1-md':
+            showDome.innerHTML = `<iframe src='./demo/md/w01/w01.html' width="100%" height="100%"/>`;
+            break;
         case 'w2':
             showDome.innerHTML = `<iframe src='./demo/w02-tictactoe/tictactoe_42.html' width="100%" height="100%"/>`;
+            break;
+        case 'w2-md':
+            showDome.innerHTML = `<iframe src='./demo/md/w02/w02.html' width="100%" height="100%"/>`;
             break;
         case 'w3':
             showDome.innerHTML = `<iframe src='./demo/w03-review/index.html' width="100%" height="100%"/>`;
             break;
+        case 'w3-md':
+            showDome.innerHTML = `<iframe src='./demo/md/w03/w03.html' width="100%" height="100%"/>`;
+            break;            
         case 'w4':
             showDome.innerHTML = `<iframe src='./demo/w04-menu/index.html' width="100%" height="100%"/>`;
             break;
+        case 'w4-md':
+            showDome.innerHTML = `<iframe src='./demo/md/w04/w04.html' width="100%" height="100%"/>`;
+            break;
         case 'w5-1':
             showDome.innerHTML = `<iframe src='./demo/w05-menu/index.html' width="100%" height="100%"/>`;
+            break;
+        case 'w5-md':
+            showDome.innerHTML = `<iframe src='./demo/md/w05/w05.html' width="100%" height="100%"/>`;
             break;
         case 'w5-2':
             showDome.innerHTML = `<iframe src='./demo/w5-modal/index.html' width="100%" height="100%"/>`;
@@ -22,15 +37,27 @@ const showClassDemo = (week) => {
         case 'w6':
             showDome.innerHTML = `<iframe src='./demo/w06-array/p3_42/p3_42.html' width="100%" height="100%"/>`;
             break;
+        case 'w6-md':
+            showDome.innerHTML = `<iframe src='./demo/md/w06/w06.html' width="100%" height="100%"/>`;
+            break;
+        case 'w9-md':
+            showDome.innerHTML = `<iframe src='./demo/md/w09/w09.html' width="100%" height="100%"/>`;
+            break;
         case 'p1':
             showDome.innerHTML = `<iframe src='./demo/midprep_stud_42/p1_bmi_42/p1_42.html' width="100%" height="100%"/>`;
             break;
         case 'p2':
-            showDome.innerHTML = `<iframe src='./demo/midprep_stud_42/p2_counter_42/p2_42.html' width="100%" height="100%"/>`;
+            showDome.innerHTML = `<iframe src='./demo/midprep_stud_42/p2_counter_42/index.html' width="100%" height="100%"/>`;
             break;
         case 'p3':
-            showDome.innerHTML = `<iframe src='./demo/midprep_stud_42/p3_tour_42/p3_42.html' width="100%" height="100%"/>`;
-            break;        
+            showDome.innerHTML = `<iframe src='./demo/midprep_stud_42/p3_tour_42/index.html' width="100%" height="100%"/>`;
+            break;
+        case 'github':
+            showDome.innerHTML = `<iframe src='https://github.com/George0113/1112-1N-js-demo-211410542' width="100%" height="100%"/>`;
+            break;
+        case 'github':
+            showDome.innerHTML = `<iframe src='https://github.com/George0113/1112-1N-js-demo-211410542/commits/main' width="100%" height="100%"/>`;
+            break;   
     }
 
     
