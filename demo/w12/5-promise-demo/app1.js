76bebeae3ec1d30c65827e18274229be769693fa
George0113 <a28935617@gmail.com>
2023-05-04 20:42:14 +0800

    W12-P3: use async/await to solve the cb hell problem

--- demo/w12/5-promise-demo/app1.js
+++ demo/w12/5-promise-demo/app1.js
@@ -5,23 +5,17 @@ const heading4 = document.querySelector('.four');
 
 const btn =  document.querySelector('.btn');
 
-btn.addEventListener('click',async ()=>{
-    const result = await displayColor();
-    console.log('result',result); 
+btn.addEventListener('click',()=>{
+    addColor(1000,heading1,'red')
+     .then(()=>{
+        return addColor(2000,heading2,'green')})
+     .then(()=>{
+        return addColor(1000,heading3,'blue')})
+     .then(()=>{
+        return addColor(500,heading4,'purple')})
+     .catch((error)=> console.log(error));
+
 });
-
-const displayColor = async ()=>{
-    try{
-        await addColor(2000,heading1,'red');
-        await addColor(2000,heading2,'green');
-        await addColor(2000,heading3,'blue');
-        await addColor(2000,heading4,'purple');
-        console.log('success');
-    }catch(error){
-        console.log(error);
-    }
-
-}
 
 const addColor =(time,element,color)=>{
     return new Promise((resolve,reject)=>{
@@ -36,7 +30,20 @@ const addColor =(time,element,color)=>{
     });
 }
 
-
+// btn.addEventListener('click', ()=>{
+//     setTimeout(()=>{
+//         heading1.style.color = 'red';
+//         setTimeout(()=>{
+//             heading2.style.color = 'green';
+//             setTimeout(()=>{
+//                 heading3.style.color = 'blue';
+//                 setTimeout(()=>{
+//                     heading4.style.color = 'purple';
+//                 },500);
+//             },1000);
+//         },2000);
+//     },1000);
+// });
 
 const promise = new Promise((resolve,reject)=>{
     let value = true;
