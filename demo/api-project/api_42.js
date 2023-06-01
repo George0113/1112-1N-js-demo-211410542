c7a9b02fde0c855db1aaf0453459c576e5ea27c5
George5 Chen(陳思語) <George5_Chen@corpnet.asus>
2023-06-01 17:31:49 +0800

    api

---
+++ demo/api-project/api_42.js
@@ -0,0 +1,36 @@
+var accessKey = "812193ef71ca946e361ed541979a0cfd91e9419a19235fd05f51ea14233f020a";
+var perPage = 30;
+
+var utm = "&utm_source=Photo-app&utm_medium=referral";
+
+var container = document.querySelector(".container");
+
+function searchPhotos() {
+  var searchInput = document.getElementById("searchInput").value;
+  var searchUrl = "https://api.unsplash.com/search/photos/?client_id=" + accessKey + "&query=" + searchInput + "&per_page=" + perPage;
+
+  makeRequest(searchUrl);
+}
+
+function makeRequest(url) {
+  container.innerHTML = ""; // 清空容器內的內容
+
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+    var results = response.results;
+
+    for (var i = 0; i < results.length; i++) {
+      if (results[i].height > results[i].width) {
+        var img = document.createElement("img");
+        img.setAttribute("src", results[i].urls.thumb + utm);
+        container.appendChild(img);
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+makeRequest("https://api.unsplash.com/photos/?client_id=" + accessKey + "&per_page=" + perPage);
